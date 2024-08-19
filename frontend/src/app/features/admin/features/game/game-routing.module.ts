5efec01b0166dab0c1e3507231dfa8b69a748c22
cofyye <cofyye@gmail.com>
2024-08-19 20:01:13 +0200

    Created Template for Game Edit Component and Made Minor Changes

--- frontend/src/app/features/admin/features/game/game-routing.module.ts
+++ frontend/src/app/features/admin/features/game/game-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
         pathMatch: 'full',
       },
       {
-        path: ':gameId',
+        path: 'edit/:gameId',
         component: GameEditComponent,
         pathMatch: 'full',
       },

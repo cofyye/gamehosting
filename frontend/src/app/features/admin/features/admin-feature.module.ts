473b5b8f1d1370a870785e08eea2b6a1d6440b88
cofyye <cofyye@gmail.com>
2024-08-24 16:05:20 +0200

    Redesigned Header and Added Navbar

--- frontend/src/app/features/admin/features/admin-feature.module.ts
+++ frontend/src/app/features/admin/features/admin-feature.module.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { LocationModule } from './location/location.module';
 import { SidebarComponent } from '../shared/components/sidebar/sidebar.component';
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { AdminIndexComponent } from './admin-index.component';
 import { RouterModule } from '@angular/router';
 import { UserModule } from './user/user.module';
 
 @NgModule({
-  declarations: [AdminIndexComponent, SidebarComponent, HeaderComponent],
+  declarations: [
+    AdminIndexComponent,
+    SidebarComponent,
+    NavbarComponent,
+    HeaderComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule,

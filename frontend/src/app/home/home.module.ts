553d76d88875cb1a2542089e76a42ab647c43cdf
cofyye <cofyye@gmail.com>
2024-08-27 21:33:32 +0200

    Designed Navbar

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, NavbarComponent],
   imports: [CommonModule, RouterModule],
 })
 export class HomeModule {}
